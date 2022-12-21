f58fef34394f8ca91fe0a9ef6a9d6b61ee8efac3
Sergej <23sergej@gmail.com>
2022-12-21 16:00:08 +0200

    Moved product page

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <?php
 
 return [
-    'production' => false,
-    'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
+    'production'  => false,
+    'baseUrl'     => '',
+    'title'       => 'Templid tools',
+    'description' => 'Templid tools - a collection of tools for business owners and entrepreneurs.',
     'collections' => [],
+
+    'productTitle' => function ($page, $title) {
+        if (!$title) {
+            return $page->title;
+        }
+
+        return sprintf('%s | %s', $title, $page->title);
+    },
 ];
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,12 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
+    .js('source/_assets/js/product.js', 'js')
     .css('source/_assets/css/main.css', 'css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+    ])
+    .css('source/_assets/css/product.css', 'css', [
         require('postcss-import'),
         require('tailwindcss'),
     ])
