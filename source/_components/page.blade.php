3e8388ad9aeebd8f7972bf6e6a4952cef7929717
Sergej <23sergej@gmail.com>
2022-12-23 19:27:09 +0200

    WIP

--- source/_components/page.blade.php
+++ source/_components/page.blade.php
@@ -6,7 +6,7 @@
 <div>
     <x-cover>
         <div class="bg-cover bg-center text-white p-10" style="background-image: url('{{ $image }}')">
-            <h1 class="text-3xl font-bold">{{ $title }}</h1>
+            <h1 class="text-3xl font-bold">{!! $title !!}</h1>
         </div>
     </x-cover>
 
