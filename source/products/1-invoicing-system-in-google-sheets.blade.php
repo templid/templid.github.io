1bee6f4ecb28bd4d84a5565ad0fc7f2f163e292d
Sergej <23sergej@gmail.com>
2022-12-23 15:06:40 +0200

    WIP

--- source/products/1-invoicing-system-in-google-sheets.blade.php
+++ source/products/1-invoicing-system-in-google-sheets.blade.php
@@ -45,7 +45,7 @@
                     </div>
                     <div id="smart-button-container">
                         <div style="text-align: center;">
-                        <div id="paypal-button-container"></div>
+                            <div id="paypal-button-container"></div>
                         </div>
                     </div>
                     <div class="border-l-8 pl-3 border-green-500 hidden" id="success-message">
@@ -57,8 +57,8 @@
                 <div class="basis-7/12">
                     <div class="flex justify-center pt-12">
                     <div class="relative h-[456px] w-full">
-                        <img class="absolute right-0 w-[540px] top-[45px] opacity-80" src="https://templid.github.io/img/invoice-source-example.png">
-                        <img class="absolute right-0 w-[420px] shadow-2xl" src="https://templid.github.io/img/invoice-example.png">
+                        <img class="absolute right-0 w-[540px] top-[45px] opacity-80" src="/assets/images/invoice-source-example.png">
+                        <img class="absolute right-0 w-[420px] shadow-2xl" src="/assets/images/invoice-example.png">
                     </div>
                     </div>
                 </div>
